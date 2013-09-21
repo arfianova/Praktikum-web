ad21f620fd617e115dff32f2486ee9a12217cf30
Pio Arfianova F. Islami <pio.arfianova@gmail.com>
2013-09-22 00:03:15 +0700

    modul 4

---
+++ 110533430584/Praktikum Ke-4/latihan1a.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head>
+	<title>Set Cookie</title> 
+</head> 
+ 
+<body> 
+	<?php
+	
+	//Men-set nilai cookie
+	setcookie('nama_cookie', 'nikai_cookie');
+	
+	//Mendapatkan nilai cookie
+	echo $_COOKIE['nama_cookie'];
+	
+	?>
+	
+	<p>
+	Tekan Refresh (F5);
+	</p>
+ 
+</body> 
+</html> 
